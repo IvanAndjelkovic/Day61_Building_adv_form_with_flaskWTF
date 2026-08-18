5334fda09d26769cb4b0da9b0a91b71cf68ac31e
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2026-08-18 06:03:24 +0000

    Finished part "Receiving Form Data with WTForms"

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ This will install the packages from requirements.txt for this project.
 
 class MyForm(FlaskForm):
     email = StringField(label='Email', validators=[DataRequired(message='Email is required.'), Email(message='That\'s not a valid email address.'), Length(min=6, max=35, message='Email must be between 6 and 35 characters long.')])
-    password = PasswordField(label='Password', validators=[DataRequired(message='Password is required.')])
+    password = PasswordField(label='Password', validators=[DataRequired(message='Password is required.'), Length(min=8, message='Password must be at least 8 characters long.')])
     submit = SubmitField(label='Log In')
 
 app = Flask(__name__)
@@ -35,10 +35,18 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = MyForm()
+
+    
     if form.validate_on_submit():
         # Process the form data
-        pass
+        print(form.email.data)
+        if form.email.data=="admin@email.com" and form.password.data=="12345678":
+            return render_template('success.html')
+        else:
+            return render_template('denied.html')
     return render_template('login.html', form=form)
+    
+    
 
 
 if __name__ == '__main__':
